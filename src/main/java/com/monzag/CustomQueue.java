042c19f3bbefb0179d73bd23a60bb3040fd446e9
monzag <zaganiacz.m@gmail.com>
2018-01-30 14:54:25 +0100

    Add first, last fields and add, getFirst method

--- src/main/java/com/monzag/CustomQueue.java
+++ src/main/java/com/monzag/CustomQueue.java
@@ -2,5 +2,22 @@ package com.monzag;
 
 public class CustomQueue {
 
+    private Node first;
+    private Node last;
 
+    public void add(int i) {
+        Node newItem = new Node(i);
+        if (first == null) {
+            first = newItem;
+            last = first;
+
+        } else {
+            last.setNext(newItem);
+            last = newItem;
+        }
+    }
+
+    public int getFirst() {
+        return first.getData();
+    }
 }
