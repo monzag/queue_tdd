a2c18f507505edc14d04940a5bdd26a55e6682b7
monzag <zaganiacz.m@gmail.com>
2018-01-30 14:55:22 +0100

    Add setup and testEnqueue

--- src/test/java/com/monzag/CustomQueueTest.java
+++ src/test/java/com/monzag/CustomQueueTest.java
@@ -1,13 +1,29 @@
 package com.monzag;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomQueueTest {
 
+    private CustomQueue queue;
+
+    @BeforeEach
+    void setup() {
+        this.queue = new CustomQueue();
+    }
+
     @Test
     void testConstructor() {
         CustomQueue queue = new CustomQueue();
+    }
+
+    @Test
+    void testEqueue() {
+        queue.add(1);
+        queue.add(2);
+        int expect = 1;
+        assertEquals(expect, queue.getFirst());
     }
 }
