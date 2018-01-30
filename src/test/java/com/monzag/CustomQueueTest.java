ed433f37999fbdfcbe6bf4728ae48caedcafb070
monzag <zaganiacz.m@gmail.com>
2018-01-30 14:31:48 +0100

    Add CustomQueueTest and testConstructor

---
+++ src/test/java/com/monzag/CustomQueueTest.java
@@ -0,0 +1,13 @@
+package com.monzag;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomQueueTest {
+
+    @Test
+    void testConstructor() {
+        CustomQueue queue = new CustomQueue();
+    }
+}
